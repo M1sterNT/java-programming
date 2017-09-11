687d6c4060aa8bc7173060c7da1fcb4f03eea891
userkmitl <59050300@kmitl.ac.th>
2017-09-11 11:53:41 +0700

    add: 01

---
+++ 01/program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class program {
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int age = 0;
+    age = scan.nextInt();
+    if(age > 60 )
+        System.out.println("Retire");
+    }
+}
