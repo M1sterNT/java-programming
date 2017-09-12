39778af52a70c2165de21e75547e8eb22bf13f9a
userkmitl <59050300@kmitl.ac.th>
2017-09-12 12:53:01 +0700

    add: 07

---
+++ 07/program.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class program {
+    public static void main(String[] args){
+       Scanner scan = new Scanner(System.in);
+       System.out.print("Amount => ");
+       int amount = scan.nextInt();
+       System.out.println("Price => ");
+       int price = scan.nextInt();
+       System.out.println("Money => ");
+       double money = scan.nextInt();
+       double total = amount * price;
+       if(total > 300) total = total - (total* 0.03);
+       total = total + (total * 0.07);
+       if(total > money) System.out.println("No Valid");
+       else System.out.println("Change : " + (money - total));
+    }
+}
